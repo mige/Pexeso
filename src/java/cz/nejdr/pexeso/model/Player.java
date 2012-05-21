79fbe6e67374336d23cf8813634b8efe96489ab8
Aleš Nejdr <ales.nejdr@gmail.com>
2012-05-22 00:39:53 +0200

    Add Player data structure

---
+++ src/java/cz/nejdr/pexeso/model/Player.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.nejdr.pexeso.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author mige
+ */
+public class Player implements Serializable{
+    
+    private String name;
+    
+    private int score;
+    
+    public Player(String name)
+    {
+        this.name = name;
+        score = 0;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public int getScore()
+    {
+        return score;
+    }
+    
+    public void increaseScore()
+    {
+        score++;
+    }
+}
